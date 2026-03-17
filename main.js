9c6b0c20e4c92f65dfeb746820e0a4f258fcb92d
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-17 23:54:08 +0530

    all the basic

--- main.js
+++ main.js
@@ -9,3 +9,54 @@ const age = 24;
 console.log("His first name is "+ firstName + "and his last name is " + lastName + ".He is age " + age + " and married: " + married);
 
 console.log(`his name is $firstName`);
+
+
+// Question
+
+let personfirstName = "Dhanush";
+
+let personlastName = "Prajapati";
+
+const greet = "Namaste";
+
+console.log(`Hello, ${personfirstName}  ${personlastName}! ${greet}`);
+
+
+//2 Question
+
+let gender = "false";
+
+if(gender === "true"){
+    console.log("male");
+} else{
+    console.log("female");
+}
+
+
+
+// 3 Question 
+
+// counting from 0 to 100 and adding them together
+
+
+for(let i=0; i<=100; i++){
+    console.log(i);
+}
+
+
+//even number in array
+
+let arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i] % 2 === )
+}
+
+
+
+
+/*Let's write some code -
+1. Write a progrßm prints all the even numbers in an array
+2. Write a program to print the biggest number in an arrya
+3. Write a program that prints all the male people's first name given a complex object
+4. Write a program that reverses all the elements of an array*/ 
