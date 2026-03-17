b4a8b465618f3c1d6e2d292b9e5f7e6b03982f43
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-17 19:32:35 +0530

    basic revision

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstName = "Ranjaya";
+
+var lastName = "Kumar";
+
+let married = false;
+
+const age = 24;
+
+console.log("His first name is "+ firstName + "and his last name is " + lastName + ".He is age " + age + " and married: " + married);
+
+console.log(`his name is $firstName`);
