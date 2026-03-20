d78727a83d847be67720558436218efb84ebba5c
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-21 00:00:02 +0530

    reading things added

--- main.js
+++ main.js
@@ -1,3 +1,92 @@
+/*1. What is a Closure in JavaScript?Closures: Function along bind with its Lexi call scope form a closures….
+
+Closures allow you to access the outer function's variables from the inner function, even after the outer function is finished executing…...
+function main() {
+
+    const name = "Dhanush";
+    function sayMyName() {
+        console.log(name)
+    }
+     sayMyName();    // this one is closure  // it will print the Dhanush
+}
+main();
+
+
+Lexical Environment :
+ local memory along  + with lexical environment of parent ………………………………….
+Lexical means (Sequence , in herecay )  : c function seating lexical inside the  function  a 
+
+function a(){
+    c();
+    function c(){
+         console.log(b);
+    }
+}
+var b=10;
+a();
+
+---------------------------------------------------------------------------------------------------------------------------------------
+
+. What is JavaScript?
+JavaScript is a programming language used to desing  interactive website and webApplication . is to give the functionality and logic to the webpage .JavaScript adds dynamic features like buttons that respond when clicked, animations, dropdown menus, and real-time updates (like showing the current time or live scores). makes a website feel alive and functional, rather than just a static page .  It is single thread even handle asynchronous task….. making them more functional and engaging.
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+2. What is a promise in JavaScript?
+• A promise is a JavaScript object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value. ,
+• Promises make it easier to handle asynchronous code, And avoiding "callback hell."
+A promise can be in one of three states: 
+	• Pending: The operation is still in progress.
+	• Fulfilled: The operation completed successfully.
+	• Rejected: The operation failed.
+Example : a person made pyment ether successful, fail₫, or in process…
+CODE :=>
+	
+
+const fetchData = new Promise((resolve, reject) => {             
+  let success = true; // Simulating success or failure
+  setTimeout(() => {
+    if (success) {
+      resolve("Data fetched successfully!");
+    } else {
+      reject("Failed to fetch data!");
+    }
+  }, 2000);
+});
+fetchData
+  .then((data) => {
+    console.log(data); // Output: Data fetched successfully!
+  })
+  .catch((error) => {
+    console.error(error); // Output: Failed to fetch data!
+  });
+
+Promise Methods
+	• .then(): Handles the result of a fulfilled promise.
+	• .catch(): Handles the result of a rejected promise.
+	• .finally(): Runs after the promise is settled (fulfilled or rejected), regardless of the outcome.
+
+
+Axios 📨
+Axios is a JavaScript library used to make HTTP requests (like GET, POST) to APIs.
+👉 Think of it as a messenger that helps your app talk to a server and get/send data.
+
+
+
+	1) What is a Closure in JavaScript?
+	
+	Closures allow us to access the outer function's variables from the inner function, even after the outer function is finished executing…...
+	function main() {
+	
+	    const name = "Dhanush";
+	    function sayMyName() {
+	        console.log(name)
+	    }
+	     sayMyName();    // this one is closure  // it will print the Dhanush
+	}
+main();*/
+
+
 let firstName = "Ranjaya";
 
 var lastName = "Kumar";
