79f932fef8d84f485aa680171b424909e2333a47
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-25 23:25:36 +0530

    lexical environment

--- index.js
+++ index.js
@@ -22,3 +22,31 @@ b();
 Window => Big object with a lot Function and Method… also create this key words… when Glogle Exexution create alog with this is created  this,
 This === window
 //hosting//
+
+
+
+Scope in js is  directly related to Lexical Environment : …………………..><
+
+
+function a(){
+    console.log(b);
+}
+var b=10;
+a();            // output: 10
+
+
+//  Var b is lies in Globle scope so can be asscess in inside the function and in nested function
+
+
+
+
+function a(){
+    c();
+    function c(){
+         console.log(b);
+    }
+}
+var b=10;           
+a();          // 10
+
+SCOPE ? => scope means where you can access à specific Variable or function in our code
