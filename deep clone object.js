3a47b342763eb4d50ad8b1795d5c6536648afd03
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-02 21:54:02 +0530

    deep clone object

---
+++ deep clone object.js
@@ -0,0 +1,13 @@
+function deepClone(obj) {
+    if (obj === null || typeof obj !== "object") {
+        return obj;
+    }
+
+    const clone = Array.isArray(obj) ? [] : {};
+
+    for (let key in obj) {
+        clone[key] = deepClone(obj[key]);
+    }
+
+    return clone;
+}
