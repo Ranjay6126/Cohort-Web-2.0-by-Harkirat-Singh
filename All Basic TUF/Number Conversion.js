2b634ccf21012f9ff5c3ea986bc87926f1609a1b
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-05 19:50:06 +0530

    data type

---
+++ All Basic TUF/Number Conversion.js
@@ -0,0 +1,21 @@
+console.log(1+2+"3"); // it will give the result by adding from let
+//  it is number then the it make calution
+
+console.log("1"+2+3); // it will not add the 
+
+
+
+let str = "Hello";
+
+str1 = str + " World";
+
+console.log(str);
+console.log(str1); // Hello World
+
+
+// symbol
+
+const id = Symbol('123');
+const s_id = Symbol('123');
+
+console.log(id == s_id); // it will give false === / == in case
