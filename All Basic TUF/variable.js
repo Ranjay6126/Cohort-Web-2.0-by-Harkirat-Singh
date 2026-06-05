dc21009de4651daf6c09a3e421c758ae1e5e8e6a
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-05 17:18:40 +0530

    all the basic tuf

---
+++ All Basic TUF/variable.js
@@ -0,0 +1,47 @@
+const accountID = 1234;
+
+let accountEmail = "panditranjay33@gmail.com";
+
+var accountPassword = "1234";
+
+accountcity = "pune"
+
+// accountID = 2345; can't be change 
+console.log(accountID);
+
+accountEmail = "ab@gmail.com"
+
+console.log(accountEmail);
+
+accountEmail = 123;
+
+console.table([accountEmail, accountPassword, accountcity]);
+
+
+
+// Type conversion 
+
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueIntNumber = Number(score);
+console.log(valueIntNumber);
+console.log(typeof valueIntNumber);
+
+let makestring = String(score);
+console.log(typeof makestring);
+
+
+let islogedin = 1;
+
+let booleanisloggedin = Boolean(islogedin);
+console.log(booleanisloggedin);
+
+
+let numberis = 44;
+
+let convertString = String(numberis);
+console.log(convertString);
+console.log(typeof convertString);
